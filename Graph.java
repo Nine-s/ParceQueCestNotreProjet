c9ad025a575cea5f51f423f8e4f4be9ca18856b3
Mitch Connor <ormagoden666.42@gmail.com>
2017-05-30 23:36:02 +0200

    Tentative d'ajout de vertex

--- Graph.java
+++ Graph.java
@@ -1,20 +1,37 @@
-import java.io.*;
-import java.util.*;
+import java.util.List;
 
 class Graph{
-  private String graphName;
-  private final List <Edge> edgeList;
-  private final List <Vertex> vertexList;
-  public List getEdge (List edge) {return edge;}
-  public List getVertex (List vertex) {return vertex;}
-  public Graph(String mygraphName){
-    graphName=myGraphName;
-  }
-  public void addVertex(String myVertexName, ){
-    
+    private final List <Edge> edgeList;
+    private final List <Vertex> vertexList;
+    public List getEdge (List edge) {return edge;}
+    public List getVertex (List vertex) {return vertex;}
+
+    public Graph(){
+	vertexList = new ArrayList<>();
+	edgeList = new ArrayList<>();
     }
-  public void addEdge(String myEdgeName, Vertex myVertexstart, Vertex myVertexend){
-    Vertex myVertex= new Vertex(myEdgeName, myVertexstart, myVertexend);
-    edgeList.add(myVertex);
-  }
+
+    /**
+     * Adds a Vertex to the graph and connects it to the vertex v
+     *
+     * @param myVertexName a <code>String</code> value
+     * @param v a <code>Vertex</code> value
+     */
+    public void addVertex(String myVertexName, Vertex v){
+	Vertex vertex = new Vertex (myVertexName);
+	vertexList.add(vertex);
+	vertex.connectToVertex(v);
+    }
+    
+    public void addVertex(String myVertexName, List<Vertex> vertices){
+	Vertex vertex = new Vertex (myVertexName);
+	vertexList.add(vertex);
+	for (Vertex v : vertices)
+	    vertex.connectToVertex(v);
+    }
+
+    public void addEdge(String myEdgeName, Vertex myVertexstart, Vertex myVertexend){
+	Vertex myVertex= new Vertex(myEdgeName, myVertexstart, myVertexend);
+	edgeList.add(myVertex);
+    }
 }
