d1cba43b60c4fde54ee516acecdc2dc1e0593efa
Mitch Connor <ormagoden666.42@gmail.com>
2017-05-30 22:59:48 +0200

    update Vertex

--- Vertex.java
+++ Vertex.java
@@ -1,6 +1,28 @@
-import java.io.*;
-import java.util.*;
 
+import java.util.List;
+import java.util.ArrayList;
 class Vertex{
-  private String vertexName;
-  private
+    private String vertexName;
+    private List <Vertex> listVertex;
+
+    public Vertex(String vertexName) {
+	this.vertexName = vertexName;
+	listVertex = new ArrayList<>();
+    }
+n
+    public String getVertexName() {
+	return vertexName;
+    }
+
+    public void setVertexName(final String vertexName) {
+	this.vertexName = vertexName;
+    }
+
+    public List <Vertex> getListVertex() {
+	return listVertex;
+    }
+
+    public void connectToVertex (Vertex vertex){
+	listVertex.add(vertex);
+    }
+
